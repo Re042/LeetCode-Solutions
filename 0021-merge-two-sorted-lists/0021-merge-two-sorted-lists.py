6f06984cb7247463df3330a65801cf2980b2ef6e
Re042 <119497871+Re042@users.noreply.github.com>
2025-04-10 13:47:46 +0300

    Time: 0 ms (100%), Space: 17.7 MB (62.19%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -7,31 +7,30 @@ class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         if not list1 or not list2:
             return list1 if not list2 else list2
-        if list1.val <= list2.val:
-            mhead = list1
-            node1 = list1.next
-            node2 = list2
-        else:
-            mhead = list2
-            node1 = list1
-            node2 = list2.next
+        mhead = ListNode()
+        prev = None
         mnode = mhead
-        while node1 or node2:
-            if node1 and node2:
-                if node1.val <= node2.val:
-                    mnode.next = node1
-                    node1 = node1.next
-                    mnode = mnode.next
-                else:
-                    mnode.next = node2
-                    node2 = node2.next
-                    mnode = mnode.next
-            elif not node1:
-                mnode.next = node2
-                node2 = node2.next
-                mnode = mnode.next
-            else:
-                mnode.next = node1
+        node1 = list1
+        node2 = list2
+        while node1 and node2:
+            if node1.val <= node2.val:
+                prev = mnode
+                mnode = node1
+                prev.next = mnode
                 node1 = node1.next
-                mnode = mnode.next
-        return mhead
+            else:
+                prev = mnode
+                mnode = node2
+                prev.next = mnode
+                node2 = node2.next
+        while node1:
+            prev = mnode
+            mnode = node1
+            prev.next = mnode
+            node1 = node1.next
+        while node2:
+            prev = mnode
+            mnode = node2
+            prev.next = mnode
+            node2 = node2.next
+        return mhead.next
